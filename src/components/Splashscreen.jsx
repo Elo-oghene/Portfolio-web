7d75fadfc0ec6349b5d2d8e9994d77cc917e8b28
Elo-oghene Olokor <Olokorelo52@gmail.com>
2024-06-21 10:33:17 +0100

    changed font

--- src/components/Splashscreen.jsx
+++ src/components/Splashscreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Splashscreen = () => {
   return (
     <div className="bg-white flex items-center justify-center h-screen">
-      <div className="text-[40px] font-semibold md:text-[65px] text-animate">Tina's Portfolio</div>
+      <div className="text-[40px] font-semibold md:text-[65px] text-animate font-roboto">TINA'S PORTFOLIO</div>
     </div>
   );
 };
